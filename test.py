5b216196e26b23b8e0b4e986c44f79ac49f378f9
DMarc06 <daninomarc@gmail.com>
2022-10-12 17:09:00 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,48 @@
+#x = 1 
+#y = 2
+#z = 0
+
+#z = x  
+#x = y 
+#y = z 
+
+#print(x)
+#print(y) 
+
+# Algorithme AffichageNomComplet
+#nom = input("entrez votre nom : ")
+#prenom = input("entrez votre prenom : ")
+#print("Votre nom complet est " , nom, prenom)
+
+# # Algorithme CalculDouble
+#a = input("entrez un nombre : ")
+#b = int(a) * 2
+#print(b)
+
+# Algorithme AffichagePrenom
+#prenom = input("entrez votre prenom : ")
+#print("Bonjour " + prenom)
+# Algorithme prix TTC 
+#PrixHT = input("Entrer le prix Ht :") 
+#TVA = input("saisir le taux de Tva :") 
+#NbArticles = input("Saisir la quantité :") 
+#TTC = input(PrixHT*NbArticles*TVA) 
+
+#nombre = input("entrez un chiffre : ")
+#if nombre > "0":
+#  print("Ce nombre est positif")
+#elif nombre == "0":
+#  print("Ce nombre est null")
+
+#else:
+#  print("Ce nombre est negatif") 
+
+
+#i= int(input("Entrez un nombre")) 
+#while i=int < 10  or  i > 20 :
+#  if i < 10 (input("plus grand")): 
+#  elif i > 20 (input("plus petit")):
+#print("yes")
+
+i= int(input("saisir un nombre"))
+print ("input")
